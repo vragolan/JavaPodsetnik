bc69f0035835d900d258779de6fa0ef5274daa93
vragolan <peckobre@gmail.com>
2019-12-22 22:32:41 +0100

    Initial commit

---
+++ src/com/primer/java/Main.java
@@ -0,0 +1,11 @@
+package com.primer.java;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Ovo je neki tekst");
+        String nekiTekst = "Filip";
+        System.out.println("Vaše ime je " + nekiTekst);
+    }
+}
+
